bebce05f50d4b255086527439f42bb5b96c66da0
Radiah12 <72045915+Radiah12@users.noreply.github.com>
2020-10-24 18:53:13 -0400

    Changed credentials another time

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,7 +2,7 @@
 $servername="s0znzigqvfehvff5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $dBUsername="n7zj77cjgp9673ei"; 
 $dBPassword="g2xz0qa3uchr136m"; 
-$dBName="mindspaceconnection";
+$dBName="s8f679033bj5vycm";
 
 
 $conn= mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
