c64b5a0c9b3d7b521f50301a6b83bfa85a6a79a5
Radiah12 <72045915+Radiah12@users.noreply.github.com>
2020-10-24 17:49:47 -0400

    Removed try-catch on top

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
-
-    try{
-        $db = parse_url(getenv("postgres://yxlicoyvhurguv:e2e8a068e4a705e7ba186fa6b1690cdf0a571da153bd867a88ef8f319e25eeb3@ec2-52-86-116-94.compute-1.amazonaws.com:5432/d8o099bsst9clv"));
-        $pdo = new PDO("pgsql:" . sprintf(
-            "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-            $db["host"],
-            $db["port"],
-            $db["user"],
-            $db["pass"],
-            ltrim($db["path"], "/")
-        ));
-    }
-    catch{
-        echo $e->getMessage();
-    }
-
-    ?>
    session_start();
 ?>
 <!DOCTYPE html> 
